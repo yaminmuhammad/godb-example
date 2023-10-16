6f8b8df35334821852bf916be2a90392c95f24d7
yaminmuhammad <1910631170108@student.unsika.ac.id>
2023-10-16 13:34:57 +0700

    add func addStudent

--- main.go
+++ main.go
@@ -15,18 +15,35 @@ const (
 	dbname   = "enigmacamp"
 )
 
-func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
+func main() {
+	addStudent("7", "Kiko", "kiko@yahoo.com", "Bogor", "1998-03-20", "M")
+}
+
+func addStudent(id, name, email, address, birthDate, gender string) {
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close() // Close the database connection when we're done with it.
+
+	defer db.Close() // Close the database connection when we'r done with it.
 	err = db.Ping()
 	if err != nil {
 		panic(err)
+	} else {
+		fmt.Println("Successfully Connected")
 	}
 	// Create a new table in the database called 'users'.
-	fmt.Println("Successfully Connected")
+
+	sqlStatement := "INSERT INTO mst_student (id, name, email, address, birth_date, gender) VALUES (" + id + "," + "'" + name + "'" + "," + "'" + email + "'" + "," + "'" + address + "'" + "," + "'" + birthDate + "'" + "," + "'" + gender + "'" + ");"
+
+	_, err = db.Exec(sqlStatement)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("Successfully Insert Data!")
+	}
+
 }
